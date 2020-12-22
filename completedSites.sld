dc4c19b1761f4cfb4c5fb5762edbb29fb049454d
Desmond Larsen-Rosner <drplr@protonmail.com>
2020-12-21 17:27:29 -0700

    Initial commit of canlin template

---
+++ completedSites.sld
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" version="1.1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.1.0/StyledLayerDescriptor.xsd" xmlns:se="http://www.opengis.net/se" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ogc="http://www.opengis.net/ogc">
+  <NamedLayer>
+    <se:Name>Canlin_EM_LIST-dec15</se:Name>
+    <UserStyle>
+      <se:Name>Canlin_EM_LIST-dec15</se:Name>
+      <se:FeatureTypeStyle>
+        <se:Rule>
+          <se:Name>Completed</se:Name>
+          <se:Description>
+            <se:Title>Completed</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:Not>
+              <ogc:PropertyIsNull>
+                <ogc:PropertyName>EM_Complet</ogc:PropertyName>
+              </ogc:PropertyIsNull>
+            </ogc:Not>
+          </ogc:Filter>
+          <se:PointSymbolizer>
+            <se:Graphic>
+              <se:Mark>
+                <se:WellKnownName>square</se:WellKnownName>
+                <se:Fill>
+                  <se:SvgParameter name="fill">#bde34b</se:SvgParameter>
+                </se:Fill>
+                <se:Stroke>
+                  <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+                  <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+                </se:Stroke>
+              </se:Mark>
+              <se:Size>7</se:Size>
+            </se:Graphic>
+          </se:PointSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:Name>to do</se:Name>
+          <se:Description>
+            <se:Title>to do</se:Title>
+          </se:Description>
+          <ogc:Filter xmlns:ogc="http://www.opengis.net/ogc">
+            <ogc:PropertyIsNull>
+              <ogc:PropertyName>EM_Complet</ogc:PropertyName>
+            </ogc:PropertyIsNull>
+          </ogc:Filter>
+          <se:PointSymbolizer>
+            <se:Graphic>
+              <se:Mark>
+                <se:WellKnownName>square</se:WellKnownName>
+                <se:Fill>
+                  <se:SvgParameter name="fill">#6032e0</se:SvgParameter>
+                </se:Fill>
+                <se:Stroke>
+                  <se:SvgParameter name="stroke">#232323</se:SvgParameter>
+                  <se:SvgParameter name="stroke-width">0.5</se:SvgParameter>
+                </se:Stroke>
+              </se:Mark>
+              <se:Size>21</se:Size>
+            </se:Graphic>
+          </se:PointSymbolizer>
+        </se:Rule>
+        <se:Rule>
+          <se:MinScaleDenominator>0</se:MinScaleDenominator>
+          <se:MaxScaleDenominator>100000</se:MaxScaleDenominator>
+          <se:TextSymbolizer>
+            <se:Label>
+              <!--SE Export for WellId || '\n' || 'EM Completed:  ' || EM_Complet not implemented yet-->Placeholder</se:Label>
+            <se:Font>
+              <se:SvgParameter name="font-family">MS Shell Dlg 2</se:SvgParameter>
+              <se:SvgParameter name="font-size">13</se:SvgParameter>
+            </se:Font>
+            <se:LabelPlacement>
+              <se:PointPlacement>
+                <se:AnchorPoint>
+                  <se:AnchorPointX>0</se:AnchorPointX>
+                  <se:AnchorPointY>0</se:AnchorPointY>
+                </se:AnchorPoint>
+              </se:PointPlacement>
+            </se:LabelPlacement>
+            <se:Halo>
+              <se:Radius>2</se:Radius>
+              <se:Fill>
+                <se:SvgParameter name="fill">#ffffff</se:SvgParameter>
+              </se:Fill>
+            </se:Halo>
+            <se:Fill>
+              <se:SvgParameter name="fill">#000000</se:SvgParameter>
+            </se:Fill>
+          </se:TextSymbolizer>
+        </se:Rule>
+      </se:FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>
---
+++ index.js
@@ -0,0 +1,128 @@
+import 'ol/ol.css';
+import 'ol-ext/control/LayerSwitcher.css';
+import BingMaps from 'ol/source/BingMaps';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import { transform } from 'ol/proj';
+import LayerGroup from 'ol/layer/Group';
+import LayerImage from 'ol/layer/Image';
+import LayerTile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS';
+import ZoomToExtent from 'ol/control/ZoomToExtent';
+import Zoom from 'ol/control/Zoom';
+import ZoomSlider from 'ol/control/ZoomSlider';
+import FullScreen from 'ol/control/FullScreen';
+import ScaleLine from 'ol/control/ScaleLine';
+import Attribution from 'ol/control/Attribution';
+import {Control, defaults as defaultControls} from 'ol/control';
+//import {Control,  as defaultControls} from 'ol/control';
+
+import LayerSwitcher from 'ol-ext/control/LayerSwitcher';
+
+var map = new Map({
+    target: 'map',
+		controls: defaultControls().extend([
+					new FullScreen(),
+					new ScaleLine()					
+					]),
+	layers: [
+        new LayerTile({
+			  visible: true,
+			  title: "Bing",
+			  preload: Infinity,
+			  source: new BingMaps({
+				key:
+				'82o22Jd9KBSXdi7KOw9F~-ljDB0Kkf0oF-Egpwvb9_w~Aqsa-2Is6gI2fOr88_Kgqe8RC041lQaheYQt9ISHnZ2L4jpJkBerWGqwZ2t31CRV',		
+				imagerySet: 'Aerial',
+				// use maxZoom 19 to see stretched tiles instead of the BingMaps
+				// "no photos at this zoom level" tiles
+				 maxZoom: 19
+			  }),
+			}),
+								 new LayerTile({
+									  				  title: 'EM31',
+										//extent: [-13884991, 2870341, -7455066, 6338219],
+										preload: Infinity,
+										visible: true,
+										source: new TileWMS({
+												  url: 'https://larsenwest.ca:8443/geoserver/Canlin/wms',
+												attributions: '© <a href="https://aksgeoscience.com" >AKS Geoscience</a>',
+													params: {
+														'LAYERS':'site_10_11_26_13_02w4_31,site_11_16_20_013_02w4_31,site_12_16_27_013_02w4_31,site_13_08_22_013_02w4_31,site_14_06_29_013_03w4_31,site_15_06_33_013_02w4_31,site_16_08_26_013_03w4_31,site_17_08_29_013_03w4_31,site_18_08_32_013_03w4_31,site_19_06_24_016_02w4_31,site_1_15_25_014_02w4_31,site_20_10_11_015_02w4_31,site_21_14_19_016_01w4_31,site_22_14_24_016_02w4_31,site_23_16_09_015_01w4_31,site_24_16_24_016_02w4_31,site_25_05_15_016_02w4_31,site_26_08_15_016_02w4_31,site_27_14_23_016_02w4_31,site_28_14_35_017_01w4_31,site_29_16_23_016_02w4_31,site_2_16_25_014_02w4_31,site_30_04_20_019_01w4_31,site_31_04_21_019_01w4_31,site_32_06_16_019_01w4_31,site_33_06_17_019_01w4_31,site_34_10_16_019_01w4_31,site_35_10-17-019-01w4_31,site_3_02_36_014_02w4_31,site_4_06_36_014_02w4_31,site_5_08_03_015_02w4_31,site_6_08_27_014_02w4_31,site_7_14_18_015_01w4_31,site_8_06_32_013_02w4_31,site_9_10_30_013_02w4_31',	
+														'TILED': true														
+													},
+										  serverType: 'geoserver',
+										//  enableOpacitySliders: true
+										//  transition: 0,
+								}), 
+					 }),
+					  new LayerTile({
+									  				  title: 'EM track',
+										//extent: [-13884991, 2870341, -7455066, 6338219],
+										preload: Infinity,
+										visible: false,
+										source: new TileWMS({
+												  url: 'https://larsenwest.ca:8443/geoserver/Canlin/wms',
+												attributions: '© <a href="https://aksgeoscience.com" >AKS Geoscience</a>',
+													params: {
+														'LAYERS':														'site_10_11_26_13_02w4_31_track,site_11_16_20_013_02w4_31_track,site_12_16_27_013_02w4_31_track,site_13_08_22_013_02w4_31_track,site_14_06_29_013_03w4_31_track,site_15_06_33_013_02w4_31_track,site_16_08_26_013_03w4_31_track,site_17_08_29_013_03w4_31_track,site_18_08_32_013_03w4_31_track,site_19_06_24_016_02w4_31_track,site_1_15_25_014_02w4_31_track,site_20_10_11_015_02w4_31_track,site_21_14_19_016_01w4_31_track,site_22_14_24_016_02w4_31_track,site_23_16_09_015_01w4_31_track,site_24_16_24_016_02w4_31_track,site_25_05_15_016_02w4_31_track,site_26_08_15_016_02w4_31_track,site_27_14_23_016_02w4_31_track,site_28_14_35_017_01w4_31_track,site_29_16_23_016_02w4_31_track,site_2_16_25_014_02w4_31_track,site_30_04_20_019_01w4_31_track,site_31_04_21_019_01w4_31_track,site_32_06_16_019_01w4_31_track,site_33_06_17_019_01w4_31_track,site_34_10_16_019_01w4_31_track,site_35_10_17_019_01w4_31_track,site_3_02_36_014_02w4_31_track,site_4_06_36_014_02w4_31_track,site_5_08_03_015_02w4_31_track,site_6_08_27_014_02w4_31_track,site_7_14_18_015_01w4_31_track,site_8_06_32_013_02w4_31_track,site_9_10_30_013_02w4_31_track',
+														'TILED': true														
+													},
+										  serverType: 'geoserver',
+										//  enableOpacitySliders: true
+										//  transition: 0,
+								}), 
+					 }),
+					  new LayerTile({
+									  				  title: 'EM38',
+										//extent: [-13884991, 2870341, -7455066, 6338219],
+										preload: Infinity,
+										visible: false,
+										source: new TileWMS({
+												  url: 'https://larsenwest.ca:8443/geoserver/Canlin/wms',
+												attributions: '© <a href="https://aksgeoscience.com" >AKS Geoscience</a>',
+													params: {
+														'LAYERS':'site_10_11_26_13_02w4_38,site_11_16_20_013_02w4_38,site_12_16_27_013_02w4_38,site_13_08_22_013_02w4_38,site_14_06_29_013_03w4_38,site_15_06_33_013_02w4_38,site_16_08_26_013_03w4_38,site_17_08_29_013_03w4_38,site_18_08_32_013_03w4_38,site_19_06_24_016_02w4_38,site_1_15_25_014_02w4_38,site_20_10_11_015_02w4_38,site_21_14_19_016_01w4_38,site_22_14_24_016_02w4_38,site_23_16_09_015_01w4_38,site_24_16_24_016_02w4_38,site_25_05_15_016_02w4_38,site_26_08_15_016_02w4_38,site_27_14_23_016_02w4_38,site_28_14_35_017_01w4_38,site_29_16_23_016_02w4_38,site_2_16_25_014_02w4_38,site_30_04_20_019_01w4_38,site_31_04_21_019_01w4_38,site_32_06_16_019_01w4_38,site_33_06_17_019_01w4_38,site_34_10_16_019_01w4_38,site_35_10_17_019_01w4_38,site_3_02_36_014_02w4_38,site_4_06_36_014_02w4_38,site_5_08_03_015_02w4_38,site_6_08_27_014_02w4_38,site_7_14_18_015_01w4_38,site_8_06_32_013_02w4_38,site_9_10_30_013_02w4_38',	
+														'TILED': true														
+													},
+										  serverType: 'geoserver',
+										//  enableOpacitySliders: true
+										//  transition: 0,
+								}), 
+					 }),
+						new LayerTile({
+									  title: 'EM LIST',
+										//extent: [-13884991, 2870341, -7455066, 6338219],
+										preload: Infinity,
+										visible: true,
+										source: new TileWMS({
+												  url: 'https://larsenwest.ca:8443/geoserver/wms',
+												// attributions: '© <a href="https://sentinel.esa.int/web/sentinel/home" >Sentinel ESA</a>',
+													params: {
+														'LAYERS': 'CANLIN_EMList', 
+														'TILED': false
+														},
+										  serverType: 'geoserver',
+										//  enableOpacitySliders: true
+										//  transition: 0,
+								}), 
+					 })
+						  
+	
+				
+		    ],
+					
+  view: new View({
+    center: [ -12260934,6510959],
+    zoom: 9	
+  }),
+   
+});
+
+   // var layerSwitcher = new ol.control.LayerSwitcher({        enableOpacitySliders: true    });
+	 var layerSwitcher = new LayerSwitcher();
+    map.addControl(layerSwitcher);
+	
+
+  
